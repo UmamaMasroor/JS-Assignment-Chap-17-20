d5f1c6bb800bfe32c7c12010a74abb779d0c2b9b
Umama Masroor <umamafatima319@gmail.com>
2023-06-20 12:53:24 -0700

    question # 3

---
+++ question # 3/add.js
@@ -0,0 +1,7 @@
+// question 3
+// Write a program to print numeric counting from 1 to 10.
+let counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < counting.length; i++) {
+  console.log(counting[i]);
+}
